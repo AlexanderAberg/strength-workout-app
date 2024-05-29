bae27c906f3cb1aae937c636b7dbf2fdec5cb086
Alexander Åberg <alexander_aberg1985@msn.com>
2024-05-29 16:10:26 +0000

    Succeeded in making the logic with help of lists and append

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -13,55 +13,32 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('strength_workout_app')
 
 
-"""
-def get_strength_data():
-  """  """
-    Get strength workout data from user
-    """"""
-    print('You can enter your workout information here.')
-    print('Sets, reps and weight needs to be written as numbers, and weight is counted in Kilograms.')
-
-    data_str = input('Enter your information here')
-    print(f'The information you provided is {data_str}')
-
-get_strength_data()
-print(get_strength_data)
-"""
-"""
-The start screen/menu
-"""
 def start():
-    strength_workout_app = StrengthWorkoutApp()
 
     while True:
-        print_separator()
         print('Welcome to the Strength Workout App')
-        print_separator()
         print('1. Workout Manager\n2. Exit')
 
         choice = input('Choose an option in the menu!')
-            """
-            The choices
-            """
+        data = SHEET.worksheet("strength").get_all_values()
+        print(data)
+        strength = []
         if choice == '1':
-            exercise = input('Enter exercise'):
-            sets = input('Enter amount of sets'):
-            reps = input('Enter amount of reps'):
-            weight = input('Enter the weight in Kilogram'):
-            strength_workout_app.log_workout(SHEET.worksheet('strength'))
-            print_separator()
+            exercise = input('Enter exercise ')
+            sets = input('Enter amount of sets ')
+            reps = input('Enter amount of reps ')
+            weight = input('Enter the weight in Kilogram ')
+            strength.extend([exercise, sets, reps, weight])
+            data.append(strength)
+            print(data)
             print(f'Exercise added')
 
         elif choice == '2':
-            print_separator()
-                print('Thanks for today, see you another time, goodbye and have a nice day!')
+            print('Thanks for today, see you another time, have a nice day!')
 
         else:
-            print_separator(
-                print('Option not possible, please press number 1 or 2')
-            )
-            
-if __name__ = '__start__':
-    main()
+            print('Option not possible, please press number 1 or 2')
 
-print(start)
+
+if __name__ == '__main__':
+    start()
